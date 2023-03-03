8f0112a173cb6fc7c186511992b72851226eaa16
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-03-03 16:07:09 +0400

    refactored login method

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\userRequest;
 use App\Models\User;
@@ -16,21 +17,22 @@ use Termwind\Components\Dd;
 
 class userController extends Controller
 {
-    
-    public function register(userRequest $post){
 
-    $con = new User();
-    $this->validate($post, [
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|min:5',
-    ],[
-        'required' => __('Fill in the required fields'),
-        'confirm-password.same' => __('Password confirmation does not match')
-    ]);
-    // $yoxla = User::where('email','=', $post->email)->orwhere('password','=', $post->pasword)->count();
+    public function register(userRequest $post)
+    {
 
-    // if($yoxla==0)
-    // {
+        $con = new User();
+        $this->validate($post, [
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5',
+        ], [
+            'required' => __('Fill in the required fields'),
+            'confirm-password.same' => __('Password confirmation does not match')
+        ]);
+        // $yoxla = User::where('email','=', $post->email)->orwhere('password','=', $post->pasword)->count();
+
+        // if($yoxla==0)
+        // {
         $con->foto = 'https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg';
         $con->blok = 0;
         $con->name = $post->name;
@@ -41,86 +43,77 @@ class userController extends Controller
         $con->password = Hash::make($post->password);
         $con->email_verification_code = Str::random(40);
         $con->save();
-        $data=[];
-        $data['email_name']='Anbar.az';
-        $data['subject']='Email verification';
-        $data['text']='Emailniz tesdiqleyin';
-        $data['link']=env('APP_URL').'/user-verification/'.$con->email_verification_code;
+        $data = [];
+        $data['email_name'] = 'Anbar.az';
+        $data['subject'] = 'Email verification';
+        $data['text'] = 'Emailniz tesdiqleyin';
+        $data['link'] = env('APP_URL') . '/user-verification/' . $con->email_verification_code;
         Mail::to($con->email)->send(new SendMail($data));
-        Auth::attempt(['email'=>$con->email,'password'=>$con->password,'blok'=>0]);
+        Auth::attempt(['email' => $con->email, 'password' => $con->password, 'blok' => 0]);
 
-        return redirect()->route('hesabla')->with('success','Qeydiyyat tammalandi zenhmet olmasa emailinizi yoxlayin!');
-    // }
-    // return back()->with('warning','Bu istifadeci artiq movcuddur!');
+        return redirect()->route('hesabla')->with('success', 'Qeydiyyat tammalandi zenhmet olmasa emailinizi yoxlayin!');
+        // }
+        // return back()->with('warning','Bu istifadeci artiq movcuddur!');
 
     }
 
-    public function user_verification(Request $request){
+    public function user_verification(Request $request)
+    {
         $verification = $request->verification;
-        $user_verification = User::where('email_verification_code',$verification)->first();
-        if($user_verification){
-            $user_verification->status=1;
-            $user_verification->email_verification_code="";
+        $user_verification = User::where('email_verification_code', $verification)->first();
+        if ($user_verification) {
+            $user_verification->status = 1;
+            $user_verification->email_verification_code = "";
             $user_verification->save();
             return view('success_verification');
-            
         }
         return redirect()->route('login');
     }
 
-    public function login(Request $post){
+    public function login(LoginRequest $request)
+    {
+        $request=$request->validated();
+        $request['blok'] = 0;
 
-        $this->validate($post,[
-            'email'=>'email|required',
-            'password'=>'required'
-
-        ]);
-
-
-        $statuslogin = User::where('email',$post->email)->where('status',1)->first();
-        if(!$statuslogin){
-            return back()->with('error','Bele bir email bazada yoxdur!');
+        if (!auth()->attempt($request)) {
+            return back()->with('error', 'Daxil etdiyiniz login ve ya parol yanlishdir');
         }
 
-        if(!Auth::attempt(['email'=>$post->email,'password'=>$post->password,'blok'=>0]))
-        {return back()->with('error','Daxil etdiyiniz login ve ya parol yanlishdir');}
-
-    return redirect()->route('hesabla');
+        return redirect()->route('hesabla');
     }
 
-    public function logout(){
+    public function logout()
+    {
         auth()->logout();
         return redirect()->route('daxilol');
     }
 
-    public function verify_email($verification_email){
-        $con = User::where('email_verification_code',$verification_email)->first();
+    public function verify_email($verification_email)
+    {
+        $con = User::where('email_verification_code', $verification_email)->first();
 
-        if(!$con){
-            return redirect()->route('register')->with('error','Email yanlishdir');
-        }
-        else{
-            if($con->email_verified_at)
-            {
-                return redirect()->route('register')->with('error','Email verified');
-            }
-            else{
+        if (!$con) {
+            return redirect()->route('register')->with('error', 'Email yanlishdir');
+        } else {
+            if ($con->email_verified_at) {
+                return redirect()->route('register')->with('error', 'Email verified');
+            } else {
                 $con->update([
-                    'email_verified_at'=>\Carbon\Carbon::now()
+                    'email_verified_at' => \Carbon\Carbon::now()
                 ]);
-                return redirect()->route('register')->with('success','Email succesfully verified');
-
+                return redirect()->route('register')->with('success', 'Email succesfully verified');
             }
         }
     }
 
-    public function test(Request $request){
-        $data=[];
-        $data['email_name']='Anbar.az';
-        $data['subject']='Email verification';
-        $data['text']='Emailniz tesdiqleyin';
+    public function test(Request $request)
+    {
+        $data = [];
+        $data['email_name'] = 'Anbar.az';
+        $data['subject'] = 'Email verification';
+        $data['text'] = 'Emailniz tesdiqleyin';
         Mail::to('qlyvaulduz@gmail.com')->send(new SendMail($data));
-        
+
         dd(1234);
     }
 }
