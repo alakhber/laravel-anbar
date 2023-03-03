d9e33c507be2658d7a64d4410c3ef0b528fc0b20
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-03-03 16:06:38 +0400

    cretaed user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'Alakhber ',
+            'surname'=>'Nakhiyev ',
+            'email'=>'nakhiyev.alakhber@gmail.com',
+            'password'=>Hash::make('admin'),
+        ]);
+    }
+}
