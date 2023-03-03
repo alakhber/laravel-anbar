8be987906367ff8884fbae26e4905b8589580fa8
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-03-03 16:28:58 +0400

    added comment

--- app/Utilities/Helpers/blade.php
+++ app/Utilities/Helpers/blade.php
@@ -1,10 +1,18 @@
 <?php
+/**
+ * $path her hansi bir fayli yoludur 
+ */
 if(!function_exists('_asset')){
     function _asset(?string $path)  
     {
         return request()->secure() ? secure_asset($path) : asset($path);
     }
 }
+/** 
+ * bu funsiyaya ehtiyac yoxdu eslinde ama ne vaxt bir yerde istifade etmek olsa 
+ * auth()->user()->name auth()->user()->surname cagirilan zaman 2 sorgu istifade olur 
+ * bu funksiya bir sorugu gondererek 2 melumati alib birlesdirir 
+*/
 if(!function_exists('_fullname')){
     function _fullname() : string  
     {
